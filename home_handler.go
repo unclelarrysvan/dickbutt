3fc9ca3a10efc99d1266a0363a1be81583b7eda6
Dean <deanfreekinggalvin@gmail.com>
2015-04-08 00:45:12 -0400

    added some shit to help you shits

--- home_handler.go
+++ home_handler.go
@@ -6,5 +6,5 @@ import (
 )
 
 func HomeHandler(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "Dick butt")
+	fmt.Fprintln(res, "Please goto http://www.dickbutt.in/[whatever you want goes here] for some dick butt fun\nI use Imgur api to grab images, credits to come.")
 }
